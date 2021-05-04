4a9d74416effa38445baa1069c0cbb14ca0517fa
Anna Wen <54281166+annawen1@users.noreply.github.com>
2021-05-04 16:13:31 -0400

    Updates

---
+++ precache-manifest.faf40ff8b1be36f5ad89472405cb6aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69ef3f53d46ef9ccc70eb73f94c5aa6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "594d1a770b6cdac12ae1",
+    "url": "./static/css/main.4b5ec30c.chunk.css"
+  },
+  {
+    "revision": "4bcadb05dd2c2d1fd1f4",
+    "url": "./static/js/2.07025ee1.chunk.js"
+  },
+  {
+    "revision": "7de8fd49d47a0c64e584918214998562",
+    "url": "./static/js/2.07025ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594d1a770b6cdac12ae1",
+    "url": "./static/js/main.9e2b9c67.chunk.js"
+  },
+  {
+    "revision": "2c819bf19ae85c12eed0272cc26a7d2d",
+    "url": "./static/js/main.9e2b9c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0874a185afce2a53bdd1",
+    "url": "./static/js/runtime-main.d56da30d.js"
+  }
+]);
